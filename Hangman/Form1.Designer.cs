2f55163cb40b8b065b0ddc26e61781ffa7e685dd
Ehsan Rassekh <rassekh.ehsan@gmail.com>
2021-02-24 11:57:20 +0330

    My first Commit

---
+++ Hangman/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.ImageIndex = 0;
+            this.label2.ImageList = this.imageList1;
+            this.label2.Location = new System.Drawing.Point(205, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 258);
+            this.label2.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1.jpg");
+            this.imageList1.Images.SetKeyName(1, "2.jpg");
+            this.imageList1.Images.SetKeyName(2, "3.jpg");
+            this.imageList1.Images.SetKeyName(3, "4.jpg");
+            this.imageList1.Images.SetKeyName(4, "5.jpg");
+            this.imageList1.Images.SetKeyName(5, "6.jpg");
+            this.imageList1.Images.SetKeyName(6, "7.jpg");
+            this.imageList1.Images.SetKeyName(7, "8.jpg");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(443, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(33, 82);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(36, 38);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Guess";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 40);
+            this.label4.TabIndex = 5;
+            this.label4.Text = " Letters\r\nGuessed";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(123, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(53, 128);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Word";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(443, 348);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Hangman Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Hangman/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        ArrayList Guessed = new ArrayList();
+        ArrayList WordGuess = new ArrayList();
+        ArrayList MyWord = new ArrayList();
+        ArrayList TrueWord = new ArrayList();
+        string[] words={"UMBRELLA","BOOK","APPLE","CAT","DOG","OUTPUT","ERROR","TABLE",
+                           "WRITE","READ","EXIT","IRAN","ANZALI","TREE","HOME","IMAGINE","PUT"};
+        string word;
+        int NumberGuess = 0,TrueGuess=0;
+
+        private void ShowWord()
+        {
+            label1.Text = "";
+            foreach (string str in WordGuess)
+                label1.Text += str;
+        }
+        private void ShoWTrueWord()
+        {
+            label1.Text = "";
+            foreach (string str in TrueWord)
+                label1.Text += str;
+        }
+        private void Restart()
+        {
+            Random number = new Random();
+            int WordNumber = number.Next(0, words.Length);
+            word = words[WordNumber];
+            NumberGuess = 0;
+            TrueGuess = 0;
+            Guessed.Clear();
+            WordGuess.Clear();
+            MyWord.Clear();
+            TrueWord.Clear();
+
+            int count = 0;
+            listBox1.Items.Clear();
+            textBox1.Text = "";
+            textBox1.Focus();
+            label1.Text = "";
+            label2.ImageIndex = NumberGuess;
+
+
+
+
+            WordGuess.Add(" | ");
+            TrueWord.Add(" | ");
+            for (int i = 0; i < word.Length; i++)
+            {
+                TrueWord.Add(word.Substring(i, 1));
+
+
+                if (count == 2)
+                {
+                    MyWord.Add("");
+                    WordGuess.Add(word.Substring(i, 1));
+                    TrueGuess++;
+                    count = 0;
+                }
+                else
+                {
+                    MyWord.Add(word.Substring(i, 1));
+                    WordGuess.Add("_");
+                    count++;
+                }
+
+                WordGuess.Add(" | ");
+                TrueWord.Add(" | ");
+
+            }
+            ShowWord();
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Restart(); 
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Close();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string w;
+            int aski,Index=0;
+            bool Repeat = false, Guess = false;
+            if(e.KeyCode==Keys.Return)
+            {
+                w=textBox1.Text;
+                if (w != "")
+                {
+                    aski = (int)Convert.ToChar(w);
+                    if (aski >= 97 && aski <= 122)
+                        aski = aski - 32;
+
+                    w = Convert.ToChar(aski).ToString();
+
+                    if (aski >= 65 && aski <= 90)
+                    {
+                        foreach (string str in Guessed)
+                            if (str == w)
+                                Repeat = true;
+                        if (!Repeat)
+                        {
+                            Guessed.Add(w);
+                            listBox1.Items.Insert(0, w);
+                            textBox1.Text = "";
+
+                            foreach (string str in MyWord)
+                            {
+                                if (str == w)
+                                {
+                                    Guess = true;
+                                    WordGuess.RemoveAt((2 * Index) + 1);
+                                    WordGuess.Insert((2 * Index) + 1, w);
+                                    TrueGuess++;
+                                }
+                                Index++;
+                            }
+                            if (Guess)
+                            {
+                                ShowWord();
+                                Guess = false;
+                            }
+                            else
+                            {
+                                NumberGuess++;
+                                label2.ImageIndex = NumberGuess;
+                            }
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please enter a new letter");
+                            textBox1.Text = "";
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a letter");
+                        textBox1.Text = "";
+                    }
+                    if (NumberGuess == 7)
+                    {
+                        
+                        ShoWTrueWord();
+                        MessageBox.Show("GAME OVER", "GAME OVER",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Restart();
+                    }
+                    if (TrueGuess == word.Length)
+                    {
+                        MessageBox.Show("YOU WIN", "YOU WIN",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        Restart();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a letter");
+                    textBox1.Text = "";
+                }
+            }
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Restart();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program is writen by EHSAN RASSEKH\nStudent Number : 9312262100\nEmail : rassekh.ehsan@gmail.com", "About",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
